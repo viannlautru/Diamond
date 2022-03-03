67c2db7749ab19c51c280aebf1d48d155e5ab73f
viannlautru <71493630+viannlautru@users.noreply.github.com>
2022-03-03 22:30:30 +0100

    Ajoutez des fichiers projet.

---
+++ DiamonDMain/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiamonDMain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
