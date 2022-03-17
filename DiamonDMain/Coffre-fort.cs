7d297704b26b263f03d26967dbd10405df3825fd
Mathias Hue <mathias.hue@hotmail.com>
2022-03-17 09:48:02 +0100

    coffre_fort

---
+++ DiamonDMain/Coffre-fort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiamonDMain
+{
+    class Coffre_fort
+    {
+        private string name;
+        private int diamonds;
+        private Dictionary<string, Carte> decouvertes;
+
+        //Contructeur
+        public Coffre_fort(string leName)
+        {
+            this.name = leName;
+            this.diamonds = 0;
+            this.decouvertes = new Dictionary<string, Carte>();
+        }
+
+        //Getters
+        public string GetName() { return this.name; }
+        public int GetDiamonds() { return this.diamonds; }
+        public Dictionary<string, Carte> GetDecouvertes() { return this.decouvertes; }
+
+        //Setters
+        public void SetDiamonds(int theDiamonds) { this.diamonds = theDiamonds; }
+    }
+}
